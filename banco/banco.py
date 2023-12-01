8dbc7b95702fe6f9744b05c80529b2d2d5c3fadd
Leo <leandroalmeida055@gmail.com>
2023-12-01 17:12:54 -0300

    O banco ja esta funcionando

--- banco/banco.py
+++ banco/banco.py
@@ -9,7 +9,7 @@ class Banco:
 
         self.sql_create_usuario = """CREATE TABLE usuario (
                                     cpf text PRIMARY KEY,
-                                    TEXT UNIQUE NOT NULL,
+                                    nome TEXT NOT NULL,
                                     senha TEXT NOT NULL,
                                     email TEXT NOT NULL,
                                     id_despesas INTEGER,
@@ -25,21 +25,31 @@ class Banco:
                                 );"""
 
 
-        self.con = sqlite3.connect('./banco.db')
+        self.con = sqlite3.connect('./banco/banco.db')
         self.cursor = self.con.cursor()
 
 
     def create_schema(self):
             self.cursor.executescript(self.sql_drop_table)
-            self.con.commit()
             self.cursor.execute(self.sql_create_usuario)
-            self.con.commit()
             self.cursor.execute(self.sql_create_despesas)
 
-    def insert(self):
-         sql = """INSERT INTO usuario()"""
+    def get_db(self):
+        return self.cursor()
+    
+    def commit(self):
+         self.con.commit()
 
-         self.cursor()
+    def insert(self, dados):
+         sql = """INSERT INTO usuario(nome, cpf, email, senha)
+                VALUES(?, ?, ?, ?)"""
+         
+         self.cursor.execute(sql, dados)
+         self.con.commit()
 
 
+if __name__ == '__main__':
+    db = Banco()
+    dados = ['Erica', '0002484132', 'erica@financas.com', '0012']
 
+    db.insert(dados)
--- financas.py
+++ financas.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from banco.banco import Banco
 
 app = Flask(__name__)
+app.secret_key = 'ksfjgne537hbigw97rtymsgu4'
 
 @app.route('/login')
 def login() -> 'html':
 	return render_template('formulario_login.html', the_title='Finanças')
 
-@app.route('/login.php', methods=['post'])
-def bemvindo() -> 'html':
-	email = request.form['email']
-	senha = request.form['senha']
-	return render_template('bemvindo.html', the_title='Finanças', email=email, senha=senha)
+@app.route('/cadastro')
+def cadastro() -> 'html':
+	return render_template('formulario_cadastro.html', the_title='Finanças')
+
+@app.route('/salvar', methods=['post'])
+def salvar():
+	dados = []
+	for k, v in request.form.items():
+		dados.append(v)
+
+	db = Banco()
+	db.insert(dados=dados)	
+
+	return '<h2>Dados salvos com sucesso!</h2>'
+
+
 
 app.run()
